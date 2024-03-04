ed5304d495aa098b86cb195c55d84b34d30ce728
Vladislav-UZH <perebziak.vladislav@gmail.com>
2024-03-04 20:20:38 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ const { message } = require('telegraf/filters');
 const config = require('./config/config');
 const BOT_TOKEN = config.BOT_TOKEN;
 
-const bot = new Telegraf(BOT_TOKEN);
+class Bot {
+  constructor(TOKEN) {
+    this.bot = new Telegraf(TOKEN);
+  }
+
+  async listen(event, listener) {
+    this.bot.on(event, listener);
+  }
+  start() {
+    this.bot.launch();
+  }
+}
+const bot = new Bot(BOT_TOKEN).bot;
 
 bot.command('quit', async ctx => {
   // Explicit usage
@@ -14,31 +26,37 @@ bot.command('quit', async ctx => {
   await ctx.leaveChat();
 });
 
-bot.on(message('text'), async ctx => {
-  // Explicit usage
-  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello Tina`);
-  //   console.log(ctx);
-  // Using context shortcut
-  //   await ctx.reply(`Hello ${ctx.state.role}`);
+// bot.on(message('text'), async ctx => {
+//   // Explicit usage
+
+//   console.log(ctx.message);
+
+//   await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello Tina`);
+//   //   console.log(ctx);
+//   // Using context shortcut
+//   //   await ctx.reply(`Hello ${ctx.state.role}`);
+// });
+
+// bot.on('callback_query', async ctx => {
+//   // Explicit usage
+//   await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+
+//   // Using context shortcut
+//   await ctx.answerCbQuery();
+// });
+
+// bot.on('inline_query', async ctx => {
+//   const result = [];
+//   // Explicit usage
+//   await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
+
+//   // Using context shortcut
+//   await ctx.answerInlineQuery(result);
+// });
+
+bot.command('src', async ctx => {
+  await ctx.reply('Hello there!');
 });
-
-bot.on('callback_query', async ctx => {
-  // Explicit usage
-  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
-
-  // Using context shortcut
-  await ctx.answerCbQuery();
-});
-
-bot.on('inline_query', async ctx => {
-  const result = [];
-  // Explicit usage
-  await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
-
-  // Using context shortcut
-  await ctx.answerInlineQuery(result);
-});
-
 bot.launch();
 
 // Enable graceful stop
---
+++ src/services/commands/base.commands.js
@@ -0,0 +1,4 @@
+class BaseCommands {
+
+  async  register()
+}
