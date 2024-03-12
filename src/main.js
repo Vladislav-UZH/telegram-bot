d432d6bd5a68c4b437245f57ac0738d2144bb58d
random-words <sivko.anton.ua@gmail.com>
2024-03-12 23:26:07 +0200

    no progress, sadly...

--- src/main.js
+++ src/main.js
@@ -1,27 +1,30 @@
 const { Telegraf } = require('telegraf');
-// const { message } = require('telegraf/filters');
-
-/**
- * робим, так, шоб можна було викроистати сервіси, але не можна було в сервісах використати бота
- */
-
 const { service } = require('./services/service');
-
-//
 
 const config = require('./config/config');
 const BOT_TOKEN = config.BOT_TOKEN;
 
 const bot = new Telegraf(BOT_TOKEN);
 
-// With Cron
-bot.command('createPoll', ctx => {
-  service.pollCron(
+// With Cron & interval
+bot.command('startPoll', ctx => {
+  // service.pollCron(
+  service.pollInterval(
     service.createPoll(
-      ctx.telegram.sendPoll(ctx.message.chat.id, 'test', ['2', '3']),
+      ctx.telegram.sendPoll(ctx.message.chat.id, 'test', ['1', '2']),
     ),
   );
+  // );
 });
+
+// With Cron
+// bot.command('createPoll', ctx => {
+//   service.pollCron(
+//     service.createPoll(
+//       ctx.telegram.sendPoll(ctx.message.chat.id, 'test', ['2', '3']),
+//     ),
+//   );
+// });
 
 // Without Cron
 // bot.command('createPoll', ctx => {
--- src/services/service.js
+++ src/services/service.js
@@ -3,13 +3,13 @@ const { CronJob } = require('cron');
 
 class Service {
   // ObjectCron
-  startCron(handler, cronTime = '') {
-    const job = new CronJob.from({
+  startCron(handler, cronTime = '* * * * * *') {
+    const job = new CronJob({
       cronTime,
       onTick: handler,
       onComplete: null,
       start: true,
-      timeZone: 'Ukraine/Kyiv',
+      timeZone: 'America/Los_Angeles',
     });
     return job;
   }
@@ -28,6 +28,16 @@ class Service {
 
   pollCron(handler) {
     this.startCron(handler, '1 * * * * *');
+  }
+
+  // в handler, який в параметрах, забивається значення функції createPoll
+  // і потім пробує викликатися всередині інтервалу, але так як там передається
+  // команда боту створити опитування(це не є функцією), то вибиває помилку
+
+  async pollInterval(handler) {
+    setInterval(async () => {
+      handler;
+    }, 1000);
   }
 
   async createPoll(context) {
